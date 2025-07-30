1f58b9182b1f13e62d68a9e7df36663a5df6c89d
Carson Crow <carson.crow@texasbar.com>
2025-07-30 15:04:59 -0500

    Basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import App from "./App.tsx";
+import About from "./pages/About.tsx";
+import Projects from "./pages/Projects.tsx";
+import Recommendations from "./pages/Recommendations.tsx";
+import Contact from "./pages/Contact.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
